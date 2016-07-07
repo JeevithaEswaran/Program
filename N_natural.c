c137fc03a78937d7a1ca68882c5ea847358155e8
JeevithaEswaran <jeevithaeswaran1@gmail.com>
2016-07-07 10:06:59 +0530

    Update N_natural.c

--- N_natural.c
+++ N_natural.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 void main()
 {
-int n,i,sum=0;
+int n,sum=0;
 clrscr();
 printf("\nEnter the last number : ");
 scanf("%d",&n);
 for(i=1;i<=n;i++)
   sum=sum+i;
-printf("Sum of natural numbers=%d",sum);
+printf("\nSum of natural numbers=%d",sum);
 getch();
 }
