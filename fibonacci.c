c781ab740847478fa620018eae1f58eae7bee164
JeevithaEswaran <jeevithaeswaran1@gmail.com>
2016-07-07 13:53:14 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -2,16 +2,19 @@
 #include<conio.h>
 void main()
 {
-  int i=1,f=1,num;
-  clrscr();
-  printf("Enter a number: ");
-  scanf("%d",&num);
-  while(i<=num)
-  {
-      f=f*i;
-      i++;
-  }
-
-  printf("Factorial of %d is: %d",num,f);
-  getch();
+int a=0,b=1,temp,n,i;
+clrscr();
+printf("\nEnter the limit: ");
+scanf("%d",&n);
+printf("\nThe fobonacci series is : ");
+printf("\n%d\n%d",a,b);
+for(i=1;i<=n-2;i++)
+{
+temp=a+b;
+a=b;
+b=temp;
+printf("\n%d",temp);
 }
+getch();
+}
+
